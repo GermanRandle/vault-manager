df000042ab85691ddb01140b7f166b1fbefe8b2e
randle <german.randle@yandex.ru>
2024-07-26 08:31:14 +0300

    build file discovered, actualized versions

--- api-gateway/app/build.gradle.kts
+++ api-gateway/app/build.gradle.kts
@@ -5,33 +5,8 @@ plugins {
     // (which facilitates building archives that serve as distributions).
     application
 
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    // Targeting the JVM.
     alias(libs.plugins.jvm)
-}
-
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
-
-dependencies {
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
-    testImplementation(libs.junit.jupiter.engine)
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
-}
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
 }
 
 application {
@@ -42,4 +17,17 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use the JUnit 5.
+    testImplementation(libs.bundles.junit)
+
+    // Use the Kotlin JUnit 5 integration.
+    testImplementation(libs.kotlin.test.junit5)
 }
