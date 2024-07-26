d84ab1ce80a6a790a5908b7532e4b88727fdc146
randle <german.randle@yandex.ru>
2024-07-26 09:59:18 +0300

    Changing subproject names (for better displaying in IDE).

--- api-gateway/settings.gradle.kts
+++ api-gateway/settings.gradle.kts
@@ -1,2 +1,8 @@
 rootProject.name = "api-gateway"
+
 include("app")
+
+// Changing subproject names (for better displaying in IDE).
+for (subproject in rootProject.children) {
+    subproject.name = "${rootProject.name}-${subproject.name}"
+}
