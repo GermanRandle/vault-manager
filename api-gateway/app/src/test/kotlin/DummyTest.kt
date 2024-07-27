e5406e07d13659b10fadae0580e6a62fac26174e
randle <german.randle@yandex.ru>
2024-07-27 08:53:09 +0300

    dummy test

---
+++ api-gateway/app/src/test/kotlin/DummyTest.kt
@@ -0,0 +1,11 @@
+package german.randle
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DummyTest {
+    @Test
+    fun `two plus two is four`() {
+        assertEquals(4, 2 + 2)
+    }
+}
