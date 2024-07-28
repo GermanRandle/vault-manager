6b35d757567adadf87e94bbf633d655fdbbc537b
randle <german.randle@yandex.ru>
2024-07-28 14:29:43 +0300

    gql mock module

--- api-gateway/app/src/main/kotlin/KtorApplicationModules.kt
+++ api-gateway/app/src/main/kotlin/KtorApplicationModules.kt
@@ -1,7 +1,11 @@
 package german.randle
 
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.operations.Query
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -20,4 +24,22 @@ private fun Application.restApiModule() {
 }
 
 private fun Application.graphQLModule() {
+    install(GraphQL) {
+        schema {
+            packages = emptyList()
+            queries = listOf(TestQuery)
+        }
+    }
+
+    routing {
+        graphQLPostRoute()
+    }
+}
+
+/**
+ * TODO: delete. For some reason, it works with [hello] method,
+ * but I need some tool to see what it looks like in a browser.
+ */
+object TestQuery : Query {
+    fun hello(): String = "hello"
 }
