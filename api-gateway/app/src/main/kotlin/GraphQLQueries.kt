b15ec5464ce72b91574a260a7d92ebf8e3440e81
randle <german.randle@yandex.ru>
2024-07-29 10:00:34 +0300

    graphiql tested

---
+++ api-gateway/app/src/main/kotlin/GraphQLQueries.kt
@@ -0,0 +1,7 @@
+package vault.manager
+
+import com.expediagroup.graphql.server.operations.Query
+
+object GraphQLQueries : Query {
+    fun validate(): String = "To be implemented!"
+}
--- api-gateway/app/src/main/kotlin/Main.kt
+++ api-gateway/app/src/main/kotlin/Main.kt
@@ -1,17 +1,42 @@
 package vault.manager
 
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.ktor.graphiQLRoute
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.routing.routing
 
 fun main() {
     embeddedServer(
         Netty,
         port = PORT,
         host = HOST,
-        module = Application::moduleAggregator,
+        module = Application::graphQLModule,
     ).start(wait = true)
+}
+
+private fun Application.graphQLModule() {
+    install(GraphQL) {
+        schema {
+            packages = emptyList()
+            queries = listOf(GraphQLQueries)
+        }
+    }
+
+    routing {
+        graphQLPostRoute(endpoint = GRAPHQL_ENDPOINT)
+        graphiQLRoute(
+            endpoint = ROOT_ENDPOINT,
+            graphQLEndpoint = GRAPHQL_ENDPOINT,
+        )
+    }
 }
 
 private const val HOST = "127.0.0.1"
 private const val PORT = 17999
+
+private const val GRAPHQL_ENDPOINT = "graphql" // default
+private const val ROOT_ENDPOINT = "/"
