849c8678664422b5782419b1bc0493977cb8b1e1
randle <german.randle@yandex.ru>
2024-08-01 07:32:37 +0300

    second microservice added

--- api-gateway/settings.gradle.kts
+++ api-gateway/settings.gradle.kts
@@ -2,11 +2,6 @@ rootProject.name = "api-gateway"
 
 include("app")
 
-// Changing subproject names (for better displaying in IDE).
-for (subproject in rootProject.children) {
-    subproject.name = "${rootProject.name}-${subproject.name}"
-}
-
 plugins {
     // Manages which repositories to download toolchains from.
     // Recommended to use in Kotlin official documentation.
