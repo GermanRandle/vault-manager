2447832f9b95848b3ff12fb042ffdc58aadbacd9
GermanRandle <german.randlee@gmail.com>
2024-10-02 08:06:59 +0300

    more comments in `settings.gradle`'s

--- vault-service/settings.gradle.kts
+++ vault-service/settings.gradle.kts
@@ -4,6 +4,6 @@ include("server")
 
 plugins {
     // Manages which repositories to download toolchains from.
-    // Recommended to use in Kotlin official documentation.
+    // Recommended to use in Kotlin official documentation + default in `gradle init`.
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
